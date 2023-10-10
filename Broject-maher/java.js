b53a9b4c402122fb4a665304df9c0a8916bb565b
Abdlkader23 <145063795+Abdlkader23@users.noreply.github.com>
2023-10-10 13:52:50 +0300

    Add files via upload

---
+++ Broject-maher/java.js
@@ -0,0 +1,39 @@
+// let img = document.querySelector('.img');
+// let btn1 =document.querySelector('.embla__button');
+// let btn2 =document.querySelector('.btn2');
+
+
+
+  
+//   const button = document.querySelector(".embla__button");
+//   button.addEventListener("click", toggleButtonTransform);
+  
+
+
+// function phones(phone) {
+//     img.src = phone
+
+
+
+
+// }
+
+
+
+let div =document.querySelector('.div1');
+let btn =document.querySelector('.icon');
+let btn1 =document.querySelector('.icons');
+
+function my(){
+    div.style.right ="0rem"
+    btn.style.left ="0rem"
+    btn.style.display="none"
+
+}
+   
+function mys(){
+    div.style.right ="50rem"
+    btn.style.display= "inline"
+    btn.style.left ="42rem"
+    
+}
